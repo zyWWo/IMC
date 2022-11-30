725975845f59eac383b6057425cb7141af6221ba
diego bruno <dbs.diegobruno@gmail.com>
2022-11-30 09:35:52 -0300

    ultima versão IMC

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculate() {
+    var formula=document.getElementById("altura").value
+    var formula=document.getElementById("peso").value
+    console.log(altura)
+    console.log(peso)
+    
+}
